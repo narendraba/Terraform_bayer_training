b6ace813343a6bb6876e31c7996feb51e70a2dac
narendraba <ballipallinarendra@gmail.com>
2024-12-17 11:28:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "Bayer_practice" {
+resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "Bayer_practice"
+    Name = "main-vpc"
   }
 }
 
-resource "aws_subnet" "subnet_Bayer_practice" {
-  vpc_id                  = aws_vpc.Bayer_practice.id
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   tags = {
@@ -20,8 +20,8 @@ resource "aws_subnet" "subnet_Bayer_practice" {
   }
 }
 
-resource "aws_internet_gateway" "igw_bayer_practice" {
-  vpc_id = aws_vpc.Bayer_practice.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
   tags = {
     Name = "main-igw"
   }
